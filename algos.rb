e52ed7f43e4b4d345698448ec34732e6317346e5
Nicolas ZERMATI <nicoolas25@gmail.com>
2012-08-20 11:32:11 +0200

    neighbors

--- algos.rb
+++ algos.rb
@@ -12,25 +12,23 @@ module Boggle
         @grid      = grid
         @dict      = dict
         @solutions = Set.new
+        @cursor    = []
+        @word      = ""
+        @used      = (0...GRID_SIZE).map{ (0...GRID_SIZE).map{ false } }
       end
 
       def solve
-        (0...GRID_SIZE).each do |i|
-          (0...GRID_SIZE).each do |j|
-            @cursor = [[i, j]]
-            @word   = @grid.matrix[i][j]
-            explore!
-          end
-        end
+        init_steps = (0...GRID_SIZE * GRID_SIZE).map{ |n| [n / GRID_SIZE, n % GRID_SIZE] }
+        explore!(init_steps)
       end
 
       private
 
-      def explore!
-        next_steps.each do |coord|
+      def explore!(steps)
+        steps.each do |coord|
           move_to!(coord)
           @solutions << @word if @word.size >= 3 && @dict.exists?(@word)
-          explore!
+          explore!(next_steps)
           move_back!
         end
       end
@@ -40,18 +38,20 @@ module Boggle
         coords = [[i-1, j], [i, j-1], [i-1, j-1], [i+1, j], [i, j+1], [i+1, j+1], [i-1, j+1], [i+1, j-1]]
         coords.delete_if do |coord|
           i, j = coord
-          i >= GRID_SIZE || i < 0 || j >= GRID_SIZE || j < 0 || @cursor.include?(coord)
+          i >= GRID_SIZE || i < 0 || j >= GRID_SIZE || j < 0 || @used[i][j] == true
         end
       end
 
       def move_to!(coord)
         @cursor << coord
         @word += @grid.matrix[coord[0]][coord[1]]
+        @used[coord[0]][coord[1]] = true
       end
 
       def move_back!
-        @cursor.pop
-        @word = @word[0..-2]
+        i, j        = @cursor.pop
+        @word       = @word[0..-2]
+        @used[i][j] = false
       end
     end
   end
