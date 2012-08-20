b459d3df82c22569032a3a5b188294ab58926aea
Nicolas ZERMATI <nicoolas25@gmail.com>
2012-08-20 12:10:09 +0200

    refactor grid / bruteforce & precompute neigbors.

--- algos.rb
+++ algos.rb
@@ -6,15 +6,12 @@ require './dict'
 module Boggle
   module Algos
     class BruteForce
-      attr_reader :solutions
+      attr_reader :solutions, :grid
 
       def initialize(grid, dict)
         @grid      = grid
         @dict      = dict
         @solutions = Set.new
-        @cursor    = []
-        @word      = ""
-        @used      = (0...GRID_SIZE).map{ (0...GRID_SIZE).map{ false } }
       end
 
       def solve
@@ -26,33 +23,13 @@ module Boggle
 
       def explore!(steps)
         steps.each do |coord|
-          move_to!(coord)
-          @solutions << @word if @word.size >= 3 && @dict.exists?(@word)
-          explore!(next_steps)
-          move_back!
+          grid.move_to!(coord)
+          @solutions << grid.word if grid.word.size >= 3 && @dict.exists?(grid.word)
+          explore!(grid.next_steps)
+          grid.move_back!
         end
       end
 
-      def next_steps
-        i, j   = @cursor.last
-        coords = [[i-1, j], [i, j-1], [i-1, j-1], [i+1, j], [i, j+1], [i+1, j+1], [i-1, j+1], [i+1, j-1]]
-        coords.delete_if do |coord|
-          i, j = coord
-          i >= GRID_SIZE || i < 0 || j >= GRID_SIZE || j < 0 || @used[i][j] == true
-        end
-      end
-
-      def move_to!(coord)
-        @cursor << coord
-        @word += @grid.matrix[coord[0]][coord[1]]
-        @used[coord[0]][coord[1]] = true
-      end
-
-      def move_back!
-        i, j        = @cursor.pop
-        @word       = @word[0..-2]
-        @used[i][j] = false
-      end
     end
   end
 end
--- grid.rb
+++ grid.rb
@@ -2,13 +2,43 @@ require './language'
 
 module Boggle
   GRID_SIZE = 4
+  NEIGHBORS = (0...GRID_SIZE).map do |i|
+    (0...GRID_SIZE).map do |j|
+      coords = [[i-1, j], [i, j-1], [i-1, j-1], [i+1, j],
+                [i, j+1], [i+1, j+1], [i-1, j+1], [i+1, j-1]]
+      coords.delete_if do |coord|
+        a, b = coord
+        a >= GRID_SIZE || a < 0 || b >= GRID_SIZE || b < 0
+      end
+    end
+  end
 
   class Grid
-    attr_reader :lang, :matrix
+    attr_reader :lang, :word
 
     def initialize(lang_sym)
       @lang   = lang_sym
       @matrix = (0...GRID_SIZE).map{(0...GRID_SIZE).map{Language.letter(@lang)}}
+      @cursor = []
+      @word   = ""
+      @used   = (0...GRID_SIZE).map{ (0...GRID_SIZE).map{ false } }
+    end
+
+    def next_steps
+      i, j = @cursor.last
+      NEIGHBORS[i][j].reject { |coord| @used[coord[0]][coord[1]] }
+    end
+
+    def move_to!(coord)
+      @cursor << coord
+      @word += @matrix[coord[0]][coord[1]]
+      @used[coord[0]][coord[1]] = true
+    end
+
+    def move_back!
+      i, j        = @cursor.pop
+      @word       = @word[0..-2]
+      @used[i][j] = false
     end
 
     def to_s
@@ -22,4 +52,5 @@ module Boggle
       s
     end
   end
+
 end
